9e89a0a1d7c1c8c15560ab781ca22da8c8055650
Alice Micheloni <alicemicheloni@tutanota.com>
2021-05-01 17:50:33 -0300

    tela de login feita

--- componentes/PaginaLogin.js
+++ componentes/PaginaLogin.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { StyleSheet, TextInput, Image } from "react-native";
 import {
     Container,
     Content,
@@ -18,12 +18,16 @@ import { LinearGradient } from "expo-linear-gradient";
 export default function PaginaLogin() {
     return (
         <Container style={styles.container}>
+            <Image
+                source={require("../assets/logo-large.png")}
+                style={{ height: 100, width: 300, alignSelf: "center" }}
+            ></Image>
             <Text style={styles.titleLogin}>Login</Text>
             <Content>
                 <Form>
                     <View style={styles.inputIndividual}>
                         <LinearGradient
-                            colors={["#FF00FF", "#00FFFF"]}
+                            colors={["#7c32ff", "#c738d8"]}
                             start={{ x: 0.0, y: 1.0 }}
                             end={{ x: 1.0, y: 0.0 }}
                             style={styles.gradient}
@@ -45,7 +49,7 @@ export default function PaginaLogin() {
 
                     <View style={styles.inputIndividual}>
                         <LinearGradient
-                            colors={["#FF00FF", "#00FFFF"]}
+                            colors={["#7c32ff", "#c738d8"]}
                             start={{ x: 0.0, y: 1.0 }}
                             end={{ x: 1.0, y: 0.0 }}
                             style={styles.gradient}
@@ -53,7 +57,7 @@ export default function PaginaLogin() {
                             <View style={styles.innerViewHack}>
                                 <Icon
                                     active
-                                    name="warning-outline"
+                                    name="key-outline"
                                     style={{ left: 10, top: 5 }}
                                 />
                                 {/*<Label style={styles.lbl}>Password</Label>*/}
@@ -68,9 +72,61 @@ export default function PaginaLogin() {
                     </View>
                 </Form>
 
-                <Button block style={{ margin: 15, marginTop: 50 }}>
-                    <Text>Sign In</Text>
+                <LinearGradient
+                    colors={["#7c32ff", "#c738d8"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 0.0 }}
+                    style={{
+                        margin: 20,
+                        marginTop: 30,
+                        padding: -3,
+                        borderRadius: 2,
+                    }}
+                >
+                    <Button
+                        block
+                        style={{
+                            backgroundColor: "transparent",
+                            top: -1,
+                            shadowColor: "#DDD",
+                            shadowOffset: 3,
+                            shadowRadius: 2,
+                        }}
+                    >
+                        <Text>Entrar</Text>
+                    </Button>
+                </LinearGradient>
+
+                <Button block style={styles.googleBtn}>
+                    <Image
+                        source={require("../assets/google-logo.png")}
+                        style={{ height: 30, width: 30 }}
+                    ></Image>
+                    <Text style={{ color: "#0665eb" }}>
+                        Entrar usando o Google
+                    </Text>
                 </Button>
+                <View style={{ flex: 1, flexDirection: "row" }}>
+                    <Button
+                        block
+                        style={[styles.meioBloco, { borderColor: "#7c32ff" }]}
+                    >
+                        <Text style={{ color: "#7c32ff" }}>Registre-se</Text>
+                    </Button>
+                    <Button
+                        block
+                        style={[
+                            styles.meioBloco,
+                            { borderColor: "#c738d8", left: "-2%" },
+                        ]}
+                    >
+                        <Text style={{ color: "#c738d8" }}>Se esqueceu?</Text>
+                    </Button>
+                </View>
+                <Image
+                    source={require("../assets/fundo-login.png")}
+                    style={{ width: 325, height: 123, alignSelf: "flex-end" }}
+                ></Image>
             </Content>
         </Container>
     );
@@ -79,12 +135,13 @@ export default function PaginaLogin() {
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#FFF",
-        marginTop: 60,
+        marginTop: 50,
     },
     titleLogin: {
         fontFamily: "Lato",
         textAlign: "center",
         fontSize: 30,
+        marginTop: 20,
     },
     arrendodadoIn: {
         borderRadius: 20,
@@ -125,5 +182,19 @@ const styles = StyleSheet.create({
 
         top: -5,
         height: 40,
+    },
+    googleBtn: {
+        margin: 20,
+        marginTop: 0,
+        borderColor: "#0665eb",
+        borderWidth: 1,
+        backgroundColor: "#FFF",
+    },
+    meioBloco: {
+        margin: 20,
+        marginTop: 0,
+        width: "40%",
+        backgroundColor: "#FFF",
+        borderWidth: 1,
     },
 });
